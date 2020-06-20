33aa27ae4dcd396953273af1383102daa2fd6ed5
aokidai <aokidaisuke91@gmail.com>
2020-06-20 18:02:24 +0900

    reupload but I wasn't checked

---
+++ Rabbit_s House/Rabbit_s House/quanli.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Rabbit_s_House
+{
+    partial class frmQuanLi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQuanLi));
+            this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sdt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenMon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Giamgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MaSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tolSpInsert = new System.Windows.Forms.ToolStripButton();
+            this.tolSpEdit = new System.Windows.Forms.ToolStripButton();
+            this.tolSpDelete = new System.Windows.Forms.ToolStripButton();
+            this.tolSpSave = new System.Windows.Forms.ToolStripButton();
+            this.tolSpCannel = new System.Windows.Forms.ToolStripButton();
+            this.tolSpSearch = new System.Windows.Forms.ToolStripButton();
+            this.tolSpPrint = new System.Windows.Forms.ToolStripButton();
+            this.txtError = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvHoaDon
+            // 
+            this.dgvHoaDon.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.TenKH,
+            this.Sdt,
+            this.TenMon,
+            this.Gia,
+            this.Giamgia,
+            this.NgayBan});
+            this.dgvHoaDon.Location = new System.Drawing.Point(12, 62);
+            this.dgvHoaDon.Name = "dgvHoaDon";
+            this.dgvHoaDon.RowHeadersWidth = 51;
+            this.dgvHoaDon.Size = new System.Drawing.Size(978, 573);
+            this.dgvHoaDon.TabIndex = 5;
+            // 
+            // STT
+            // 
+            this.STT.HeaderText = "STT";
+            this.STT.Name = "STT";
+            this.STT.Width = 50;
+            // 
+            // TenKH
+            // 
+            this.TenKH.HeaderText = "Tên khách Hàng";
+            this.TenKH.MinimumWidth = 6;
+            this.TenKH.Name = "TenKH";
+            this.TenKH.Width = 200;
+            // 
+            // Sdt
+            // 
+            this.Sdt.HeaderText = "Số Điện Thoại";
+            this.Sdt.MinimumWidth = 6;
+            this.Sdt.Name = "Sdt";
+            this.Sdt.Width = 175;
+            // 
+            // TenMon
+            // 
+            this.TenMon.HeaderText = "Tên Món";
+            this.TenMon.MinimumWidth = 6;
+            this.TenMon.Name = "TenMon";
+            this.TenMon.Width = 175;
+            // 
+            // Gia
+            // 
+            this.Gia.HeaderText = "Giá Tiền";
+            this.Gia.MinimumWidth = 6;
+            this.Gia.Name = "Gia";
+            this.Gia.Width = 125;
+            // 
+            // Giamgia
+            // 
+            this.Giamgia.HeaderText = "Giảm Giá";
+            this.Giamgia.MinimumWidth = 6;
+            this.Giamgia.Name = "Giamgia";
+            // 
+            // NgayBan
+            // 
+            this.NgayBan.HeaderText = "Ngày Bán";
+            this.NgayBan.Name = "NgayBan";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaSP,
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(1005, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 573);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // MaSP
+            // 
+            this.MaSP.HeaderText = "Mã Sản Phẩm";
+            this.MaSP.Name = "MaSP";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Sản phẩm";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Giá Thành";
+            this.Column2.Name = "Column2";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tolSpInsert,
+            this.tolSpEdit,
+            this.tolSpDelete,
+            this.tolSpSave,
+            this.tolSpCannel,
+            this.tolSpSearch,
+            this.tolSpPrint});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1360, 47);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tolSpInsert
+            // 
+            this.tolSpInsert.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpInsert.Image = global::Rabbit_s_House.Properties.Resources.icons8_挿入_60;
+            this.tolSpInsert.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpInsert.Name = "tolSpInsert";
+            this.tolSpInsert.Size = new System.Drawing.Size(44, 44);
+            this.tolSpInsert.Text = "Insert";
+            // 
+            // tolSpEdit
+            // 
+            this.tolSpEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpEdit.Image = global::Rabbit_s_House.Properties.Resources.icons8_鉛筆_48;
+            this.tolSpEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpEdit.Name = "tolSpEdit";
+            this.tolSpEdit.Size = new System.Drawing.Size(44, 44);
+            this.tolSpEdit.Text = "Edit";
+            // 
+            // tolSpDelete
+            // 
+            this.tolSpDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpDelete.Image = global::Rabbit_s_House.Properties.Resources.icons8_削除_52;
+            this.tolSpDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpDelete.Name = "tolSpDelete";
+            this.tolSpDelete.Size = new System.Drawing.Size(44, 44);
+            this.tolSpDelete.Text = "Delete";
+            // 
+            // tolSpSave
+            // 
+            this.tolSpSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpSave.Image = global::Rabbit_s_House.Properties.Resources.icons8_セーブ_50;
+            this.tolSpSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpSave.Name = "tolSpSave";
+            this.tolSpSave.Size = new System.Drawing.Size(44, 44);
+            this.tolSpSave.Text = "Save";
+            // 
+            // tolSpCannel
+            // 
+            this.tolSpCannel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpCannel.Image = global::Rabbit_s_House.Properties.Resources.icons8_元に戻す_48;
+            this.tolSpCannel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpCannel.Name = "tolSpCannel";
+            this.tolSpCannel.Size = new System.Drawing.Size(44, 44);
+            this.tolSpCannel.Text = "Cannel";
+            // 
+            // tolSpSearch
+            // 
+            this.tolSpSearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpSearch.Image = global::Rabbit_s_House.Properties.Resources.icons8_ファイルの表示_52;
+            this.tolSpSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpSearch.Name = "tolSpSearch";
+            this.tolSpSearch.Size = new System.Drawing.Size(44, 44);
+            this.tolSpSearch.Text = "Search";
+            // 
+            // tolSpPrint
+            // 
+            this.tolSpPrint.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tolSpPrint.Image = global::Rabbit_s_House.Properties.Resources.icons8_プリント_48;
+            this.tolSpPrint.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tolSpPrint.Name = "tolSpPrint";
+            this.tolSpPrint.Size = new System.Drawing.Size(44, 44);
+            this.tolSpPrint.Text = "Print";
+            // 
+            // txtError
+            // 
+            this.txtError.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtError.Location = new System.Drawing.Point(12, 642);
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(1331, 13);
+            this.txtError.TabIndex = 8;
+            // 
+            // frmQuanLi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Rabbit_s_House.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(1360, 661);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvHoaDon);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmQuanLi";
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHoaDon;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaSP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sdt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenMon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Giamgia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgayBan;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tolSpInsert;
+        private System.Windows.Forms.ToolStripButton tolSpEdit;
+        private System.Windows.Forms.ToolStripButton tolSpDelete;
+        private System.Windows.Forms.ToolStripButton tolSpSave;
+        private System.Windows.Forms.ToolStripButton tolSpCannel;
+        private System.Windows.Forms.ToolStripButton tolSpSearch;
+        private System.Windows.Forms.ToolStripButton tolSpPrint;
+        private System.Windows.Forms.TextBox txtError;
+    }
+}
---
+++ Rabbit_s House/Rabbit_s House/quanli.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rabbit_s_House
+{
+    public partial class frmQuanLi : Form
+    {
+        public frmQuanLi()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
